f352380ef3f6f8eba6f5dbcfe6b117d6c0b5c26e
김수연 <onemask@naver.com>
2020-06-20 19:39:02 +0900

    [적용] MainActivity에 BaseActivity 적용, DataBinding 적용

--- project/app/src/main/java/com/soo/github/ui/MainActivity.kt
+++ project/app/src/main/java/com/soo/github/ui/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.soo.github.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.soo.github.R
+import com.soo.github.base.BaseActivity
+import com.soo.github.databinding.ActivityMainBinding
+import com.soo.github.ui.vm.MainViewModel
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(layoutRes = R.layout.activity_main) {
+
+    @Inject
+    lateinit var viewModelFactory : ViewModelProvider.Factory
+
+    override val viewModel : MainViewModel by viewModels { viewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun setupViewModel() {
+        binding.vm = viewModel
+
+    }
+
+
 }
