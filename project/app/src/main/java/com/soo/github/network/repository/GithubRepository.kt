cd51c72ebe0d2ea066c4a3e67944323efaeac6e4
김수연 <onemask@naver.com>
2020-06-20 16:47:54 +0900

    [수정] GithubRepository 수정

--- project/app/src/main/java/com/soo/github/network/repository/GithubRepository.kt
+++ project/app/src/main/java/com/soo/github/network/repository/GithubRepository.kt
@@ -1,8 +1,15 @@
 package com.soo.github.network.repository
 
 import com.soo.github.network.model.User
+import com.soo.github.network.remote.GithubApiService
+import com.soo.github.network.remote.GithubRemoteDataSource
 import io.reactivex.Single
 
-interface GithubRepository{
-    fun getUserList() : Single<List<User>>
+class GithubRepository(private val githubApiService: GithubApiService) :
+    GithubRemoteDataSource {
+    override fun getUserList(): Single<List<User>> {
+       //TODO("localRepository랑 분리 필요")
+      return  githubApiService.getUserList()
+    }
+
 }
