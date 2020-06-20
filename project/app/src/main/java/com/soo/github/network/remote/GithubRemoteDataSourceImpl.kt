bf695aefdba8cd3c200b42b9f6b173256a203f6a
김수연 <onemask@naver.com>
2020-06-20 16:30:59 +0900

    [삭제] 필요없는 remoteimpl 삭제

--- project/app/src/main/java/com/soo/github/network/remote/GithubRemoteDataSourceImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.soo.github.network.remote
-
-import com.soo.github.network.model.User
-import io.reactivex.Single
-
-class GithubRemoteDataSourceImpl(private val apiServiceService : GithubRemoteApiService) : GithubRemoteDataSource{
-    override fun getUserList(): Single<List<User>> = apiServiceService.getUserList()
-
-}
