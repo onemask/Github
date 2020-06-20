c86a25c64f58a8379f9f15c20702e11df1836e06
김수연 <onemask@naver.com>
2020-06-20 19:37:48 +0900

    [추가] Base Activity, BaseViewModel 추가

---
+++ project/app/src/main/java/com/soo/github/base/BaseActivity.kt
@@ -0,0 +1,26 @@
+package com.soo.github.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import dagger.android.AndroidInjection
+import dagger.android.support.DaggerAppCompatActivity
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int
+) : DaggerAppCompatActivity() {
+
+    protected lateinit var binding: B
+    abstract val viewModel: BaseViewModel
+
+    abstract fun setupViewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+        setupViewModel()
+    }
+}
---
+++ project/app/src/main/java/com/soo/github/base/BaseViewModel.kt
@@ -0,0 +1,22 @@
+package com.soo.github.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+
+    protected val disposable = CompositeDisposable()
+    protected val _loadingState = MutableLiveData<Boolean>()
+
+    val loadingState: LiveData<Boolean>
+        get() = _loadingState
+
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+
+}
