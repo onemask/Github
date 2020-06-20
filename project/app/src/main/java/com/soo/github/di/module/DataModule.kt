c4066bb234796ededdbd5c84d05c0b99f76c80e2
김수연 <onemask@naver.com>
2020-06-20 16:47:04 +0900

    [추가] DataModule 추가

---
+++ project/app/src/main/java/com/soo/github/di/module/DataModule.kt
@@ -0,0 +1,15 @@
+package com.soo.github.di.module
+
+import com.soo.github.network.remote.GithubApiService
+import com.soo.github.network.remote.GithubRemoteDataSource
+import com.soo.github.network.repository.GithubRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DataModule {
+    @Provides
+    @Singleton
+    fun provideGithubRepository(apiService: GithubApiService) : GithubRemoteDataSource = GithubRepository(apiService)
+}
