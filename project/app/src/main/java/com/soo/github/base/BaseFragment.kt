abc2dec2e7027c63b0c1330be85abb47354a1404
김수연 <onemask@naver.com>
2020-06-21 16:30:42 +0900

    [추가]BaseFragment 추가

---
+++ project/app/src/main/java/com/soo/github/base/BaseFragment.kt
@@ -0,0 +1,30 @@
+package com.soo.github.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import dagger.android.support.DaggerFragment
+
+abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutRes: Int) :
+    DaggerFragment() {
+
+    protected lateinit var binding: B
+    abstract val viewModel: BaseViewModel
+
+    abstract fun setupViewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+}
