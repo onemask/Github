8ef9baea3075b57d7c0123c89ebcecf21f6eddd1
김수연 <onemask@naver.com>
2020-06-21 14:36:37 +0900

    [추가] MainActivity 에 UserAdpater 연동

--- project/app/src/main/java/com/soo/github/ui/MainActivity.kt
+++ project/app/src/main/java/com/soo/github/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package com.soo.github.ui
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.soo.github.R
 import com.soo.github.base.BaseActivity
 import com.soo.github.databinding.ActivityMainBinding
+import com.soo.github.ui.adapter.UsersAdapter
 import com.soo.github.ui.vm.MainViewModel
 import javax.inject.Inject
 
@@ -14,16 +16,29 @@ class MainActivity : BaseActivity<ActivityMainBinding>(layoutRes = R.layout.acti
     @Inject
     lateinit var viewModelFactory : ViewModelProvider.Factory
 
+    private val adapter = UsersAdapter()
+
     override val viewModel : MainViewModel by viewModels { viewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupAdapter()
+        setupBind()
     }
 
     override fun setupViewModel() {
         binding.vm = viewModel
 
+    }
+
+    private fun setupBind(){
+        viewModel.userList.observe(this, Observer {
+            adapter.setData(it)
+        })
+    }
+
+    private fun setupAdapter(){
+        binding.rvUserList.adapter = adapter
     }
 
 
