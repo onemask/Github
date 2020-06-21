38b1808665370869d99cfc241de1fdc7b11982a6
김수연 <onemask@naver.com>
2020-06-21 16:31:34 +0900

    [수정] ActivityBinder에 FragmentProvider 수정

--- project/app/src/main/java/com/soo/github/di/module/ActivityBinder.kt
+++ project/app/src/main/java/com/soo/github/di/module/ActivityBinder.kt
@@ -2,13 +2,13 @@ package com.soo.github.di.module
 
 import com.soo.github.di.scope.PerActivity
 import com.soo.github.ui.MainActivity
-import com.soo.github.ui.di.MainActivityModule
+import com.soo.github.ui.di.MainFragmentProvider
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityBinder {
     @PerActivity
-    @ContributesAndroidInjector(modules = [MainActivityModule::class])
+    @ContributesAndroidInjector(modules = [MainFragmentProvider::class])
     abstract fun bindMainActivity(): MainActivity
 }
