11063ee5fce662f541e2bdca8a49784a06ef4620
김수연 <onemask@naver.com>
2020-06-23 00:12:11 +0900

    [추가] ViewPagerAdapter에 Repository Fragment 추가

--- project/app/src/main/java/com/soo/github/ui/userdetail/adapter/ViewPagerAdapter.kt
+++ project/app/src/main/java/com/soo/github/ui/userdetail/adapter/ViewPagerAdapter.kt
@@ -4,14 +4,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.soo.github.ui.userdetail.UserOverViewFragment
+import com.soo.github.ui.userdetail.UserRepositoryFragment
 
 class ViewPagerAdapter(fragmentManager: FragmentManager, userName: String) : FragmentPagerAdapter(
     fragmentManager,
     BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
 ) {
-
     private var titles = mutableListOf("OVERVIEW", "REPOSITORIES", "STARRED")
-    private val fragmentList = listOf(UserOverViewFragment.newInstance(userName))
+
+    private val fragmentList = listOf(
+        UserOverViewFragment.newInstance(userName),
+        UserRepositoryFragment.newInstance(userName)
+    )
 
     override fun getPageTitle(position: Int): CharSequence? {
         return titles[position]
