9eda02550bb97033ee3407f2bc8a954c7c42de44
김수연 <onemask@naver.com>
2020-06-24 00:28:15 +0900

    [추가] ViewPagerAdapter 안에 name,position 받도록 수정

--- project/app/src/main/java/com/soo/github/ui/userdetail/adapter/ViewPagerAdapter.kt
+++ project/app/src/main/java/com/soo/github/ui/userdetail/adapter/ViewPagerAdapter.kt
@@ -1,29 +1,37 @@
 package com.soo.github.ui.userdetail.adapter
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.soo.github.constants.Constants.POSITION
+import com.soo.github.constants.Constants.USERNAME
 import com.soo.github.ui.userdetail.UserOverViewFragment
-import com.soo.github.ui.userdetail.UserRepositoryFragment
-import com.soo.github.ui.userdetail.UserStarredFragment
+import com.soo.github.ui.userdetail.UserRepoAndStarredFragment
 
-class ViewPagerAdapter(fragmentManager: FragmentManager, userName: String) : FragmentPagerAdapter(
-    fragmentManager,
-    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-) {
+class ViewPagerAdapter(fragmentManager: FragmentManager, val userName: String) :
+    FragmentPagerAdapter(
+        fragmentManager,
+        BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+    ) {
     private var titles = mutableListOf("OVERVIEW", "REPOSITORIES", "STARRED")
 
-    private val fragmentList = listOf(
-        UserOverViewFragment.newInstance(userName),
-        UserRepositoryFragment.newInstance(userName),
-        UserStarredFragment.newInstance(userName)
+    private val fragmentList: List<Fragment> = listOf(
+        UserOverViewFragment.newInstance(),
+        UserRepoAndStarredFragment.newInstance(),
+        UserRepoAndStarredFragment.newInstance()
     )
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return titles[position]
-    }
+    override fun getPageTitle(position: Int): CharSequence? = titles[position]
 
-    override fun getItem(position: Int): Fragment = fragmentList[position]
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position].apply {
+            arguments = Bundle().apply {
+                putInt(POSITION, position)
+                putString(USERNAME, userName)
+            }
+        }
+    }
 
     override fun getCount(): Int = fragmentList.count()
 
