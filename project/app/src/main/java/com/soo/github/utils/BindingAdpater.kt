df4f4ca9334afba3f81499a68d8d2258f81d8141
김수연 <onemask@naver.com>
2020-06-24 00:31:29 +0900

    [추가] BindingAdapter에 replaceItems 추가

--- project/app/src/main/java/com/soo/github/utils/BindingAdpater.kt
+++ project/app/src/main/java/com/soo/github/utils/BindingAdpater.kt
@@ -2,9 +2,22 @@ package com.soo.github.utils
 
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.facebook.drawee.view.SimpleDraweeView
+import com.soo.github.base.BaseAdapter
 
 @BindingAdapter("bind:srcCompat")
 fun srcCompat(view: SimpleDraweeView, url: String) {
     view.setImageURI(url.toUri(), view.context)
+}
+
+@BindingAdapter("bind:replaceItems")
+fun replaceItems(recyclerView: RecyclerView, item: List<Any>?) {
+    if (item == null) return
+
+    @Suppress("UNCHECKED_CAST")
+    (recyclerView.adapter as BaseAdapter<Any>).run {
+        replaceItems(item)
+        notifyDataSetChanged()
+    }
 }
