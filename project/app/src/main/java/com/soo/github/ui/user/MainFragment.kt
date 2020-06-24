65525a7702f9fc86eac6b4d07f3b96b2a22c43f7
soo <onemask@stunitas.com>
2020-06-24 17:44:02 +0900

    [개발중] BaseAdapter을 적용하여 개발 중

--- project/app/src/main/java/com/soo/github/ui/user/MainFragment.kt
+++ project/app/src/main/java/com/soo/github/ui/user/MainFragment.kt
@@ -1,13 +1,18 @@
 package com.soo.github.ui.user
 
 import android.os.Bundle
+import android.util.ArrayMap
+import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.soo.github.R
+import com.soo.github.base.BaseAdapter
 import com.soo.github.base.BaseFragment
+import com.soo.github.base.BaseViewModel
 import com.soo.github.constants.Constants
 import com.soo.github.databinding.FragmentMainBinding
+import com.soo.github.network.model.User
 import com.soo.github.ui.user.adapter.UsersAdapter
 import com.soo.github.ui.user.vm.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -35,12 +40,20 @@ class MainFragment : BaseFragment<FragmentMainBinding>(layoutRes = R.layout.frag
     }
 
     private fun setupAdapter() {
-        binding.rvUserList.adapter = adapter
+        val madapter = BaseAdapter<User>(
+            layoutBindingId = Pair(R.layout.item_user, BR.user),
+            viewModels = ArrayMap<Int, BaseViewModel>().apply {
+                put(BR.vm, viewModel)
+            }
+        )
+        binding.rvUserList.adapter = madapter
+
+       // binding.rvUserList.adapter = adapter
     }
 
     private fun setupObserve() {
         viewModel.userList.observe(viewLifecycleOwner, Observer {
-            adapter.setData(it)
+          //  adapter.setData(it)
         })
 
         //TODO("BackButton 을 구현 하기 위해서는 singleEvent 정의 ")
